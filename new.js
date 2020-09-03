84eb5a0dc7fb10117b6c3acc0db17a83746163ab
softwarepassion <massbtech531@gmail.com>
2020-09-04 00:07:55 +0530

    saves

---
+++ new.js
@@ -0,0 +1,40 @@
+/*var details={
+	name:"Apssdc",
+	age:5,
+	Courses:["Web design using React Js","js","Data analysis"]
+}
+for (var i = 0; i < details.Courses.length 0; i++) 
+{
+	document.writeln(details.Courses[i]);
+}
+
+for (var i in details.Courses) {
+	document.write(details.Courses[i]+"<br>")
+}
+document.write("<br>")
+for (var i  in details) {
+	document.write(i+"<br>")
+}
+document.write("<br>")
+//don't work for objects(for of for)
+var name="APSSDC React JS"
+for (var i of name){
+	document.write(i);
+}*/
+
+
+
+//Loops
+var a=25;
+if(a<21)
+{
+	document.write("success");
+}
+else if(a==25)
+{
+	document.write("your age is"+a);
+}
+else
+{
+	document.write("Failes");
+}
